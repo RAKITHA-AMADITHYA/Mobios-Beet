2ec881650adb3471d6c26a526f7b37a5f00e7c6c
K.K.RAKITHA AMADITHYA <137595471+RAKITHA-AMADITHYA@users.noreply.github.com>
2024-06-08 17:49:34 +0530

    modify product card styules

--- src/components/product card/ProductCard.jsx
+++ src/components/product card/ProductCard.jsx
@@ -1,5 +1,4 @@
 import { Button, Card, Grid, Typography } from '@mui/material'
-import keels from '../../assets/beet/keels.png'
 import Rating from '@mui/material/Rating';
 import { useState } from 'react';
 const ProductCard = ({image,name}) => {
@@ -7,7 +6,7 @@ const ProductCard = ({image,name}) => {
   return (
     <>
     <Card >
-        <img src={image} alt="" />
+        <img src={image} style={{minWidth:'255px',maxWidth:'255px',minHeight:'290px',maxHeight:'290px'}} alt="" />
         <Grid p={2}>
            <Typography variant='h5' fontWeight={600}>{name}</Typography>
            <div style={{display:'flex', justifyContent:'space-between'}}>
