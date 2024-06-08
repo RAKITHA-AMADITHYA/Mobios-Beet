b909df6c8755de92ff02ad8d167cc95414507db0
K.K.RAKITHA AMADITHYA <137595471+RAKITHA-AMADITHYA@users.noreply.github.com>
2024-06-08 17:30:33 +0530

    created the product card component

--- src/components/product card/ProductCard.jsx
+++ src/components/product card/ProductCard.jsx
@@ -2,17 +2,17 @@ import { Button, Card, Grid, Typography } from '@mui/material'
 import keels from '../../assets/beet/keels.png'
 import Rating from '@mui/material/Rating';
 import { useState } from 'react';
-const ProductCard = () => {
+const ProductCard = ({image,name}) => {
     const[value,setValue]=useState(4);
   return (
     <>
     <Card >
-        <img src={keels} alt="" />
+        <img src={image} alt="" />
         <Grid p={2}>
-           <Typography variant='h5' fontWeight={600}>KEELS</Typography>
+           <Typography variant='h5' fontWeight={600}>{name}</Typography>
            <div style={{display:'flex', justifyContent:'space-between'}}>
              <Rating name='read-only' value={value}/>
-             <Button></Button>
+             <Button variant='contained'>ADD</Button>
            </div>
         </Grid>
        
